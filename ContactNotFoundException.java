38ea5a76597096a673bfff01fc5bc3fa509869fe
Shubham Patil <154824445+ShubhamPatil-4@users.noreply.github.com>
2024-09-10 17:04:08 +0530

    Add files via upload

---
+++ ContactNotFoundException.java
@@ -0,0 +1,12 @@
+package miniProject2;
+
+public class ContactNotFoundException extends Exception {
+
+	public ContactNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
