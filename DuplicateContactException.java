67ad45adb871d9d7e8b2be89f910e8a4a9897f32
Shubham Patil <154824445+ShubhamPatil-4@users.noreply.github.com>
2024-09-10 17:04:34 +0530

    Add files via upload

---
+++ DuplicateContactException.java
@@ -0,0 +1,12 @@
+package miniProject2;
+
+public class DuplicateContactException extends Exception {
+
+	public DuplicateContactException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
