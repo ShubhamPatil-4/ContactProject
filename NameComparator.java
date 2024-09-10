8340696c005029fd323ec333b81ef71d8bb40f8b
Shubham Patil <154824445+ShubhamPatil-4@users.noreply.github.com>
2024-09-10 17:05:56 +0530

    Add files via upload

---
+++ NameComparator.java
@@ -0,0 +1,12 @@
+package miniProject2;
+
+import java.util.Comparator;
+
+public class NameComparator implements Comparator<Contact> {
+	@Override
+	public int compare(Contact o1, Contact o2) {
+		// TODO Auto-generated method stub
+		return o1.getName().compareTo(o2.getName());
+	}
+
+}
