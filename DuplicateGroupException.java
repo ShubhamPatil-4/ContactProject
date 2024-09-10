b0faa922a2cf119f445ce1a7f48dccc2a4a73cc6
Shubham Patil <154824445+ShubhamPatil-4@users.noreply.github.com>
2024-09-10 17:04:27 +0530

    Add files via upload

---
+++ DuplicateGroupException.java
@@ -0,0 +1,13 @@
+package miniProject2;
+
+public class DuplicateGroupException extends Exception {
+
+	public DuplicateGroupException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+}
