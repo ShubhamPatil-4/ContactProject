5dc95798832302269865375abd47955153808bf8
Shubham Patil <154824445+ShubhamPatil-4@users.noreply.github.com>
2024-09-10 17:05:26 +0530

    Add files via upload

---
+++ IllegalArgumentException.java
@@ -0,0 +1,11 @@
+package miniProject2;
+
+public class IllegalArgumentException extends Exception{
+
+	public IllegalArgumentException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
