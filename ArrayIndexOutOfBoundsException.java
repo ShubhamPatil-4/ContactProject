4c97fd68639d952d09d2f380667bd8f02cfa6dc9
Shubham Patil <154824445+ShubhamPatil-4@users.noreply.github.com>
2024-09-10 17:04:47 +0530

    Add files via upload

---
+++ ArrayIndexOutOfBoundsException.java
@@ -0,0 +1,12 @@
+package miniProject2;
+
+public class ArrayIndexOutOfBoundsException extends Exception{
+
+	public ArrayIndexOutOfBoundsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
